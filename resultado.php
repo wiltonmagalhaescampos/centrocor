cf8cfc819fec9e6c49dcd62bef8116ce0c601204
AndersonAngelo <anderson_angelo1@hotmail.com>
2022-10-26 10:41:42 -0300

    layout update

--- resultado.php
+++ resultado.php
@@ -1,143 +1,292 @@
 <?php
-    session_start();
-    include_once('config.php');
-    // print_r($_SESSION);
-    if((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true))
-    {
-        unset($_SESSION['usuario']);
-        unset($_SESSION['senha']);
-        header('Location: index.php');
-    }
-    $logado = $_SESSION['usuario'];
-    if(!empty($_GET['search']))
-    {
-        $data = $_GET['search'];
-        $sql = "SELECT * FROM arquivos WHERE id LIKE '%$data%' or nome LIKE '%$data%' or prontuario LIKE '%$data%' or nome_documento LIKE '%$data%' ORDER BY id DESC";
-    }
-    else
-    {
-        $sql = "SELECT * FROM arquivos ORDER BY id DESC";
-    }
-    //$result = $conn->query($sql);
+session_start();
+include_once('config.php');
+// print_r($_SESSION);
+if ((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true)) {
+  unset($_SESSION['usuario']);
+  unset($_SESSION['senha']);
+  header('Location: index.php');
+}
+$logado = $_SESSION['usuario'];
+if (!empty($_GET['search'])) {
+  $data = $_GET['search'];
+  $sql = "SELECT * FROM arquivos WHERE id LIKE '%$data%' or nome LIKE '%$data%' or prontuario LIKE '%$data%' or nome_documento LIKE '%$data%' ORDER BY id DESC";
+} else {
+  $sql = "SELECT * FROM arquivos ORDER BY id DESC";
+}
+//$result = $conn->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>consulta paciente</title>
-    <style>
-        body{
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-            color: white;
-            text-align: center;
-            
-        }
-        .table-bg{
-            background: rgba(0, 0, 0, 0.3);
-            border-radius: 15px 15px 0 0;
-        }
+  <meta charset="UTF-8">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>consulta paciente</title>
+  <style>
+    body {
+      font-family: Roboto, sans-serif;
+      margin: 0;
+      padding: 0;
+      background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+      box-sizing: border-box;
+      color: white;
+    }
 
-        .box-search{
-            display: flex;
-            justify-content: center;
-            gap: .1%;
-        }
+    .header {
+      background: linear-gradient(to right, rgb(249, 249, 249), rgb(17, 54, 71));
+      height: 150px;
+    }
 
-        .Input{
-            position: absolute;
-            color: white;
-            top: 26%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: rgba(0, 0, 0, 0.3);
-            padding: 10px;
-            border-radius: 15px;
-        }
-    </style>
+    .user-container {
+      transition: .2s ease;
+      width: 100px;
+      margin-right: 20px;
+      align-items: center;
+    }
+
+    .icon-user {
+      background: white;
+      cursor: pointer;
+      margin-bottom: 4px;
+      border-radius: 50%;
+      width: 50px;
+    }
+
+    .container {
+      width: 100vw;
+    }
+
+    .container-main {
+      height: 50px;
+      padding: 0;
+      margin-top: 10px;
+    }
+
+    .content-list {
+      align-items: center;
+    }
+
+    ul {
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    ul>li {
+      height: 30px;
+      padding: 10px;
+      margin: 10px 5px;
+      list-style: none;
+    }
+
+    li>a {
+      color: white;
+      text-decoration: none;
+      transition: transform .2s ease;
+    }
+
+    .menu:hover {
+      color: white;
+      border-bottom: .6px solid white;
+      transform: scale(1.1);
+    }
+
+    .img-logo {
+      margin-left: 30px;
+      width: 8rem;
+    }
+
+    .bnt-logout {
+      display: none;
+
+    }
+
+    button>a {
+      color: white;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
+    .table-bg {
+      background: rgba(0, 0, 0, 0.3);
+      border-radius: 15px 15px 0 0;
+    }
+
+    .box-search {
+      display: flex;
+      justify-content: center;
+      gap: .1%;
+    }
+
+    .container-list-exams {
+      width: 80%;
+    }
+
+    .Input {
+      position: absolute;
+      color: white;
+      top: 26%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: rgba(0, 0, 0, 0.3);
+      padding: 10px;
+      border-radius: 15px;
+    }
+  </style>
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="sistema.php">Consulta paciente</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+  <div class="container-fluid p-0 m-0">
+    <header class="header d-flex justify-content-between navbar navbar-expand-lg navbar-dark">
+      <div class="">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/Logo_CENTROCOR-01.png" class="img-logo" alt="Logo centrocor">
+        </a>
+      </div>
+      <div class="user-container d-flex flex-column justify-content-center">
+        <img id="user" class="icon-user" src="./assets/usuario.png" alt="Icone de usuário">
+        <?php
+        echo "<h6>$logado</h6>";
+        ?>
+        <div class="bnt-logout-container d-flex">
+          <button onclick="logout()" type="button" class="bnt-logout btn btn-danger">
+            Sair
+          </button>
         </div>
-        <div class="d-flex">
-        <a href="resultado.php" class="btn btn-warning me-5">Vizualizar</a>
-        <a href="home.php" class="btn btn-warning me-5">Voltar</a>
-        <a href="importar.php" class="btn btn-danger me-5">Importar</a>
-        <a href="formulario.php" class="btn btn-light me-5">Adicionar</a>
-        <a href="sair.php" class="btn btn-danger me-5">Sair</a>
-            
-        </div>
+      </div>
+    </header>
+
+    <nav class="container-main d-flex flex-column">
+      <div class="content-list d-flex justify-content-evenly">
+        <ul class="d-flex flex-row">
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="sistema.php">
+              Consultar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="formulario.php">
+              Cadastrar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="usuarios.php">
+              Cadastrar Usuarios
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="imprimir.php">
+              Imprimir Exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="">
+              frase Doctor
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="">
+              Codigos de exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="importar.php">
+              Importar
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="pdf.php">
+              Gerar PDF
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="visualizar_arquivo.php">
+              visualizar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="resultado.php">
+              Gerar Resultados
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
+  </div>
+
+  <main>
     <br>
     <?php
-        echo "<h4>Usuário <u>$logado</u></h4>";
-        
-        // if (isset($_SESSION['msg'])){
-        //     echo $_SESSION['msg'];
-        //     unset ($_SESSION['msg']);
-            
-        //}
+    // if (isset($_SESSION['msg'])){
+    //     echo $_SESSION['msg'];
+    //     unset ($_SESSION['msg']);
+    //}
     ?>
     <br>
     <div class="box-search">
-        <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
-        <button onclick="searchData()" class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-        </button>
+      <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+      <button onclick="searchData()" class="btn btn-primary">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </button>
     </div>
-    </nav>
-
-<br><br>
-<?php
-include_once ('conexao.php');
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Listar</title>
-</head>
-
-<body>
-
-    <h1>Listar exames</h1>
-
-    
-
-<table class="table table-dark table-striped">
-<?php
+    <div class="container-list-exams d-flex flex-column align-items-center py-4">
+      <h1>Listar exames</h1>
+      <table class="table table-dark table-striped">
+        <?php
         $query_arquivos = "SELECT id, prontuario, nome_documento FROM arquivos ORDER BY id DESC";
-        $result_arquivos = $conn->prepare($query_arquivos);
-        $result_arquivos->execute();
+        //$result_arquivos = $conn->prepare($query_arquivos);
+        //$result_arquivos->execute();
+        // if (($result_arquivos) and ($result_arquivos->rowCount() != 0)) {
+        //   while ($row_arquivo = $result_arquivos->fetch(PDO::FETCH_ASSOC)) {
+        //     var_dump($row_arquivo);
+        //     extract($row_arquivo);
+        //     echo "ID: $id <br>";
+        //     echo "Número do prontuario: $prontuario <br>";
+        //     echo "Exame: <a href='visualizar_arquivo.php?id=$id' target='_blank'>$nome_documento</a> <br>";
+        //     echo "<hr>";
+        //   }
+        // } else {
+        //   echo "<p style='color: #f00;'>Erro: Nenhum arquivo encontrado!</p>";
+        // }
+        ?>
+      </table>
+    </div>
+  </main>
 
-        if(($result_arquivos) and ($result_arquivos->rowCount() != 0)){
-            while($row_arquivo = $result_arquivos->fetch(PDO::FETCH_ASSOC)){
-                //var_dump($row_arquivo);
-                extract($row_arquivo);
-                echo "ID: $id <br>";
-                echo "Número do prontuario: $prontuario <br>";
-                echo "Exame: <a href='visualizar_arquivo.php?id=$id' target='_blank'>$nome_documento</a> <br>";
-                echo "<hr>";
-            }
-        }else{
-            echo "<p style='color: #f00;'>Erro: Nenhum arquivo encontrado!</p>";
-        }
-    ?>
-
-        
 
 
+  <script>
+    let user = document.querySelector("#user");
+    let button = document.querySelector(".bnt-logout")
 
+    user.addEventListener("click", function() {
+      if (button.style.display === "none") {
+        button.style.display = "block"
+      } else {
+        button.style.display = "none"
+      }
+    })
+
+
+    function logout() {
+
+
+      let conf = confirm('Deseja sair?')
+
+      if (conf === true) {
+        window.location.href = "sair.php"
+      } else {
+        return;
+      }
+    }
+  </script>
 
 </body>
 
--- usuarios.php
+++ usuarios.php
@@ -1,96 +1,181 @@
 <?php
 
-    session_start();
-    //print_r($_SESSION);
+session_start();
+//print_r($_SESSION);
 
-        if((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true))
-{
-    unset($_SESSION['usuario']);
-    unset($_SESSION['senha']);
-    header('Location: index.php');
+if ((!isset($_SESSION['usuario']) == true) and (!isset($_SESSION['senha']) == true)) {
+  unset($_SESSION['usuario']);
+  unset($_SESSION['senha']);
+  header('Location: index.php');
 }
 
-        $locado = $_SESSION['usuario'];
+$locado = $_SESSION['usuario'];
+$logado = $_SESSION['usuario'];
 
 
 
-    if(isset($_POST['submit']))
-{
-   // print_r('Data do exame: ' . $_POST['data_exame']);
-   // print_r('<br>');
-   // print_r('idade: ' . $_POST['idade']);
-   // print_r('<br>');
-   // print_r('Nome do paciente: ' . $_POST['nome']);
-   // print_r('<br>');
-   // print_r('Pocedimento: ' . $_POST['procedimento']);
-   // print_r('<br>');
-   // print_r('Endereço: ' . $_POST['endereco']);
-   // print_r('<br>');
-   // print_r('Telefone: ' . $_POST['telefone']);
-   // print_r('<br>');
-   // print_r('Email: ' . $_POST['email']);
-   // print_r('<br>');
-   // print_r('tipo de procedimento: ' . $_POST['exames']);
+if (isset($_POST['submit'])) {
+  // print_r('Data do exame: ' . $_POST['data_exame']);
+  // print_r('<br>');
+  // print_r('idade: ' . $_POST['idade']);
+  // print_r('<br>');
+  // print_r('Nome do paciente: ' . $_POST['nome']);
+  // print_r('<br>');
+  // print_r('Pocedimento: ' . $_POST['procedimento']);
+  // print_r('<br>');
+  // print_r('Endereço: ' . $_POST['endereco']);
+  // print_r('<br>');
+  // print_r('Telefone: ' . $_POST['telefone']);
+  // print_r('<br>');
+  // print_r('Email: ' . $_POST['email']);
+  // print_r('<br>');
+  // print_r('tipo de procedimento: ' . $_POST['exames']);
 
-   include_once('config.php');
+  include_once('config.php');
 
-        $data_cadastro =  $_POST['data_cadastro'];
-        $usuario = $_POST['usuario'];
-        $senha = $_POST['senha'];
-        $nome = $_POST['nome'];
-        $unidade = $_POST['local'];
+  $data_cadastro =  $_POST['data_cadastro'];
+  $usuario = $_POST['usuario'];
+  $senha = $_POST['senha'];
+  $nome = $_POST['nome'];
+  $unidade = $_POST['local'];
 
-        
 
-    $result = mysqli_query($conexao, "INSERT INTO usuarios (data_cadastro,usuario,senha,nome,unidade) VALUES ('$data_cadastro','$usuario','$senha','$nome','$unidade')");
 
+  $result = mysqli_query($conexao, "INSERT INTO usuarios (data_cadastro,usuario,senha,nome,unidade) VALUES ('$data_cadastro','$usuario','$senha','$nome','$unidade')");
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de usuario</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Cadastro de usuario</title>
 </head>
 <style>
-body{
-    
-    font-family: Arial, Helvetica, sans-serif;
-    background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-}
-.box{
-    position: absolute;
+  body {
+    font-family: Roboto, sans-serif;
+    margin: 0;
+    padding: 0;
+    background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+    box-sizing: border-box;
     color: white;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  }
+
+  .header {
+    background: linear-gradient(to right, rgb(249, 249, 249), rgb(17, 54, 71));
+    height: 150px;
+  }
+
+  .user-container {
+    transition: .2s ease;
+    width: 100px;
+    margin-right: 20px;
+    align-items: center;
+  }
+
+  .icon-user {
+    background: white;
+    cursor: pointer;
+    margin-bottom: 4px;
+    border-radius: 50%;
+    width: 50px;
+  }
+
+  .container {
+    width: 100vw;
+  }
+
+  .container-main {
+    height: 50px;
+    padding: 0;
+    margin-top: 10px;
+  }
+
+  .content-list {
+    align-items: center;
+  }
+
+  ul {
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  ul>li {
+    height: 30px;
+    padding: 10px;
+    margin: 10px 5px;
+    list-style: none;
+  }
+
+  li>a {
+    color: white;
+    text-decoration: none;
+    transition: transform .2s ease;
+  }
+
+  .menu:hover {
+    color: white;
+    border-bottom: .6px solid white;
+    transform: scale(1.1);
+  }
+
+  .img-logo {
+    margin-left: 30px;
+    width: 8rem;
+  }
+
+  .bnt-logout {
+    display: none;
+
+  }
+
+  button>a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+  }
+
+  .box {
+    color: white;
+    width: 100%;
+  }
+
+  fieldset {
+    border: 3px solid dodgerblue;
+
+  }
+
+  .form-user {
     background-color: rgba(0, 0, 0, 0.3);
     padding: 10px;
-    border-radius: 15px; 
-    width: 95%;
-}
+    width: 80%;
+  }
 
-fieldset{
-    border: 3px solid dodgerblue;
-    border-radius: 20px;
-
-}
-legend{
+  legend {
     border: 1px solid dodgerblue;
     padding: 10px;
     text-align: center;
     background-color: dodgerblue;
-    border-radius: 20px;
     color: white;
-}
-.inputBox{
+  }
+
+  .inputBox {
     position: relative;
-}
-.inputUser{
+  }
+
+  .inputBox>label {
+    padding-left: 4px;
+  }
+
+  .inputUser {
     background: none;
     border: none;
     border-bottom: 1px solid white;
@@ -100,31 +185,32 @@ legend{
     width: 100%;
     letter-spacing: 1px;
 
-}
-.labelInput{
+  }
+
+  .labelInput {
     position: absolute;
     top: 0%;
     left: 0%;
     pointer-events: none;
     transition: .5s;
-}
+  }
 
-.inputUser:focus ~ .labelInput,
-.inputUser:valid ~ .labelInput{
+  .inputUser:focus~.labelInput,
+  .inputUser:valid~.labelInput {
     top: -20px;
     font-size: 12px;
     color: dodgerblue;
-}
+  }
 
-#data_exame {
+  #data_exame {
     border: none;
     padding: 8px;
     border-radius: 10px;
     outline: none;
     font-size: 15px;
-}
+  }
 
-#submit{
+  #submit {
     background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
     width: 100%;
     border: none;
@@ -132,85 +218,185 @@ legend{
     color: white;
     font-size: 15px;
     cursor: pointer;
+
+  }
+
+  #submit:hover {
+    background-image: linear-gradient(to right, rgb(0, 80, 172), rgb(80, 19, 195));
+  }
+
+  .voltar {
+
+    background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+    border: none;
+    padding: 15px;
+    color: white;
+    font-size: 15px;
+    cursor: pointer;
     border-radius: 10px;
 
-}
-#submit:hover{
-    background-image: linear-gradient(to right, rgb(0, 80, 172),rgb(80, 19, 195));
-}
+  }
 
-.voltar{
-
-background-image: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-width: 100%;
-border: none;
-padding: 15px;
-color: white;
-font-size: 15px;
-cursor: pointer;
-border-radius: 10px;
-
-}
-
-.voltar:hover{
-background-image: linear-gradient(to right, rgb(0, 80, 172),rgb(80, 19, 195));
-}
-
-
+  .voltar:hover {
+    background-image: linear-gradient(to right, rgb(0, 80, 172), rgb(80, 19, 195));
+  }
 </style>
-<body>
 
-    <div class="box">
-        <form action="usuarios.php" method="POST">
-            <fieldset>
-           
-           
-                <legend class="legend"><b>Cadastro de Usuario</b></legend>
-           
-            <br><br>
-
-
-                    <label for="data_cadastro">Data do cadastro</label> 
-                    <input type="date" name="data_cadastro" id="data_cadastro" required>
-                 
-            <br><br><br><br>
-                <div class="inputBox">
-                    <input type="text" name="usuario" id="usuario" class="inputUser" required>
-                    <label for="usuario" class="labelInput">Usuario</label>
-                </div>
-            <br><br>
-                <div class="inputBox">
-                    <input type="password" name="senha" id="senha" class="inputUser" required>
-                    <label for="senha" class="labelInput">Senha</label>
-                </div>
-            <br><br>
-                <div class="inputBox">
-                    <input type="text" name="nome" id="nome" class="inputUser" required>
-                    <label for="nome" class="labelInput">Nome sobrenome</label>
-                </div>
-            <br><br>
-
-            <p>Unidade</p>
-                    <div>
-                        <input type="radio" id="matriz" name="local" value="matriz" required>
-                        <label for="matriz">Matriz</label>
-                        
-                        <input type="radio" id="filial" name="local" value="filial" required>
-                        <label for="filial">Filial</label>
-
-                    </div>
-            <br><br>
-
-                        <div><input type="submit" name="submit" id="submit" value="Cadastrar">
-                        <br><br><br>
-                        <a class='voltar' href="home.php">Voltar</a>  
-                        </div>
-                        
-            </fieldset>
-        </form>
+<body class="d-flex flex-column">
+  <div class="">
+    <header class="header d-flex justify-content-between navbar navbar-expand-lg navbar-dark">
+      <div class="">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/Logo_CENTROCOR-01.png" class="img-logo" alt="Logo centrocor">
+        </a>
       </div>
+      <div class="user-container d-flex flex-column justify-content-center">
+        <img id="user" class="icon-user" src="./assets/usuario.png" alt="Icone de usuário">
+        <?php
+        echo "<h6>$logado</h6>";
+        ?>
+        <div class="bnt-logout-container d-flex">
+          <button onclick="logout()" type="button" class="bnt-logout btn btn-danger">
+            Sair
+          </button>
+        </div>
+      </div>
+    </header>
 
+    <main class="container-main d-flex flex-column">
+      <div class="content-list d-flex justify-content-evenly">
+        <ul class="d-flex flex-row">
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="sistema.php">
+              Consultar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="formulario.php">
+              Cadastrar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="usuarios.php">
+              Cadastrar Usuarios
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="imprimir.php">
+              Imprimir Exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="">
+              frase Doctor
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="">
+              Codigos de exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="importar.php">
+              Importar
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="pdf.php">
+              Gerar PDF
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="visualizar_arquivo.php">
+              visualizar exames
+            </a>
+          </li>
+          <li class="d-flex align-items-center justify-content-center">
+            <a class="menu" href="resultado.php">
+              Gerar Resultados
+            </a>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+
+
+  <div class="box d-flex align-items-center justify-content-center pt-5">
+    <form class="form-user" action="usuarios.php" method="POST">
+      <fieldset>
+        <legend class="legend"><b>Cadastro de Usuario</b></legend>
+
+        <br><br>
+
+
+        <label class="px-1" for="data_cadastro">Data do cadastro</label>
+        <input type="date" name="data_cadastro" id="data_cadastro" required>
+
+        <br><br><br><br>
+        <div class="inputBox">
+          <input type="text" name="usuario" id="usuario" class="inputUser" required>
+          <label for="usuario" class="labelInput">Usuario</label>
+        </div>
+        <br><br>
+        <div class="inputBox">
+          <input type="password" name="senha" id="senha" class="inputUser" required>
+          <label for="senha" class="labelInput">Senha</label>
+        </div>
+        <br><br>
+        <div class="inputBox">
+          <input type="text" name="nome" id="nome" class="inputUser" required>
+          <label for="nome" class="labelInput">Nome sobrenome</label>
+        </div>
+        <br><br>
+
+        <p class="px-1">Unidade</p>
+        <div class="px-1">
+          <input type="radio" id="matriz" name="local" value="matriz" required>
+          <label for="matriz">Matriz</label>
+
+          <input type="radio" id="filial" name="local" value="filial" required>
+          <label for="filial">Filial</label>
+
+        </div>
+        <br><br>
+
+        <div>
+          <input type="submit" name="submit" id="submit" value="Cadastrar">
+        </div>
+
+      </fieldset>
+    </form>
+  </div>
+  
+
+  <script>
+    let user = document.querySelector("#user");
+    let button = document.querySelector(".bnt-logout")
+
+    user.addEventListener("click", function() {
+      if (button.style.display === "none") {
+        button.style.display = "block"
+      } else {
+        button.style.display = "none"
+      }
+    })
+
+
+    function logout() {
+
+
+      let conf = confirm('Deseja sair?')
+
+      if (conf === true) {
+        window.location.href = "sair.php"
+      } else {
+        return;
+      }
+    }
+  </script>
 
 
 </body>
+
 </html>
